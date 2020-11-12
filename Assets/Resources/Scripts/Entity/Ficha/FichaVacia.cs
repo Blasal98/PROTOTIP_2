a720f9d0963e515b0b7e7d6db8a3c8c723fc65bf
Victor Blas <victor.blas@enti.cat>
2020-11-12 22:53:47 +0100

    Sprite de Ficha Cambiado

--- Assets/Resources/Scripts/Entity/Ficha/FichaVacia.cs
+++ Assets/Resources/Scripts/Entity/Ficha/FichaVacia.cs
@@ -10,6 +10,6 @@ public class FichaVacia : Ficha
         if (!_local) { gameObject.transform.localScale = new Vector3((float)42 / 78,0.5f,1); }
         gameObject.transform.SetParent(_parent.transform);
         gameObject.GetComponent<SpriteRenderer>().sprite =
-            UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/ficha"), new Rect(0, 0, Constants.Entity.Ficha.wPix, Constants.Entity.Ficha.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit);
+            UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Ficha/FichaVacia"), new Rect(0, 0, Constants.Entity.Ficha.wPix, Constants.Entity.Ficha.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit);
     }
 }
--- Assets/Resources/Scripts/Entity/Ficha/Tierra/Camino.cs
+++ Assets/Resources/Scripts/Entity/Ficha/Tierra/Camino.cs
@@ -7,6 +7,6 @@ public class Camino : Ficha
     public Camino()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = 
-            UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/ficha"), new Rect(0, 0, Constants.Entity.Ficha.wPix, Constants.Entity.Ficha.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit);
+            UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Ficha/"), new Rect(0, 0, Constants.Entity.Ficha.wPix, Constants.Entity.Ficha.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit);
     }
 }
