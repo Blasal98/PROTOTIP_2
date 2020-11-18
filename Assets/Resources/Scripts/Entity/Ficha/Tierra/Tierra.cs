ac4252b4f6dd19a532b0b52b1424d35386d2c5f6
Victor Blas <victor.blas@enti.cat>
2020-11-18 20:20:28 +0100

    NAIIIIS

--- Assets/Resources/Scripts/Entity/Ficha/Tierra/Tierra.cs
+++ Assets/Resources/Scripts/Entity/Ficha/Tierra/Tierra.cs
@@ -14,17 +14,22 @@ public class Tierra : Ficha
     public static List<Sprite> car_sprites;
     public static List<Sprite> tank_sprites;
     public static List<Sprite> plane_sprites;
-    public static bool spritesCreated = false;
 
     protected Tierra()
     {
-        if (!spritesCreated)
+        if (soldier_sprites == null)
         {
             soldier_sprites = new List<Sprite>();
+            car_sprites = new List<Sprite>();
+            tank_sprites = new List<Sprite>();
+            plane_sprites = new List<Sprite>();
 
-            for(int i= 0; i < 10; i++)
+            for (int i= 0; i < 10; i++)
             {
                 soldier_sprites.Add(UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Troop/Soldier/soldier" + i.ToString()), new Rect(0, 0, Constants.Entity.Troop.wPix, Constants.Entity.Troop.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit));
+                car_sprites.Add(UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Troop/Car/car" + i.ToString()), new Rect(0, 0, Constants.Entity.Troop.wPix, Constants.Entity.Troop.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit));
+                tank_sprites.Add(UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Troop/Tank/tank" + i.ToString()), new Rect(0, 0, Constants.Entity.Troop.wPix, Constants.Entity.Troop.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit));
+                plane_sprites.Add(UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Images/Troop/Plane/plane" + i.ToString()), new Rect(0, 0, Constants.Entity.Troop.wPix, Constants.Entity.Troop.hPix), new Vector2(0.5f, 0.5f), Constants.Entity.imgDfltPiXUnit));
             }
         }
 
@@ -32,12 +37,33 @@ public class Tierra : Ficha
         soldierIndicator.AddComponent<SpriteRenderer>();
         soldierIndicator.GetComponent<SpriteRenderer>().sprite = soldier_sprites[0];
         soldierIndicator.transform.SetParent(gameObject.transform);
-        soldierIndicator.transform.position = new Vector3(soldierIndicator.transform.position.x - Constants.Entity.Troop.scale / 2,
-                                                          soldierIndicator.transform.position.y + Constants.Entity.Troop.scale / 2, Constants.Layers.zFichaIndicator);
-
+        soldierIndicator.transform.position = new Vector3(soldierIndicator.transform.position.x - Constants.Entity.Troop.scale / 2.5f,
+                                                          soldierIndicator.transform.position.y + Constants.Entity.Troop.scale / 2.5f, Constants.Layers.zFichaIndicator);
         soldierIndicator.transform.localScale = new Vector3(Constants.Entity.Troop.scale, Constants.Entity.Troop.scale, 1);
 
+        carIndicator = new GameObject("CarIndicator");
+        carIndicator.AddComponent<SpriteRenderer>();
+        carIndicator.GetComponent<SpriteRenderer>().sprite = car_sprites[0];
+        carIndicator.transform.SetParent(gameObject.transform);
+        carIndicator.transform.position = new Vector3(carIndicator.transform.position.x + Constants.Entity.Troop.scale / 2.5f,
+                                                      carIndicator.transform.position.y + Constants.Entity.Troop.scale / 2.5f, Constants.Layers.zFichaIndicator);
+        carIndicator.transform.localScale = new Vector3(Constants.Entity.Troop.scale, Constants.Entity.Troop.scale, 1);
 
+        tankIndicator = new GameObject("TankIndicator");
+        tankIndicator.AddComponent<SpriteRenderer>();
+        tankIndicator.GetComponent<SpriteRenderer>().sprite = tank_sprites[0];
+        tankIndicator.transform.SetParent(gameObject.transform);
+        tankIndicator.transform.position = new Vector3(tankIndicator.transform.position.x + Constants.Entity.Troop.scale / 2.5f,
+                                                       tankIndicator.transform.position.y - Constants.Entity.Troop.scale / 2.5f, Constants.Layers.zFichaIndicator);
+        tankIndicator.transform.localScale = new Vector3(Constants.Entity.Troop.scale, Constants.Entity.Troop.scale, 1);
+
+        planeIndicator = new GameObject("PlaneIndicator");
+        planeIndicator.AddComponent<SpriteRenderer>();
+        planeIndicator.GetComponent<SpriteRenderer>().sprite = plane_sprites[0];
+        planeIndicator.transform.SetParent(gameObject.transform);
+        planeIndicator.transform.position = new Vector3(planeIndicator.transform.position.x - Constants.Entity.Troop.scale / 2.5f,
+                                                        planeIndicator.transform.position.y - Constants.Entity.Troop.scale / 2.5f, Constants.Layers.zFichaIndicator);
+        planeIndicator.transform.localScale = new Vector3(Constants.Entity.Troop.scale, Constants.Entity.Troop.scale, 1);
 
         type = Ficha_Type.TIERRA;
         troops = new List<Troop>();
